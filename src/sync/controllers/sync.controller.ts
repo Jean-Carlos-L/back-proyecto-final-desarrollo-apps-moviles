ec3aae3a1990f4f994949c687f363dafdbda1a18
Steven <maxpowersga@gmail.com>
2024-06-02 15:03:47 -0500

    Sincronizacion de base de datos remota

--- src/sync/controllers/sync.controller.ts
+++ src/sync/controllers/sync.controller.ts
@@ -3,12 +3,26 @@ import syncServices from "../services/sync.services"
 
 const getQuerysToSyncLocal = async (_: Request, res: Response) => {
     try {
-        const querys = await syncServices.updateRempoteQueries();
-        res.status(200).json(querys);
+        const queries = await syncServices.updateRempoteQueries();
+        res.status(200).json(queries);
     } catch (error) {
         console.error(error)
-        res.status(500).json('Error in login function');
+        res.status(500).json('Error in getQuerysToSyncLocal function');
     }
 }
 
-export default { getQuerysToSyncLocal };
+const getQuerysToSyncRemote = async (req: Request, res: Response) => {
+    try {
+        const localQueries = req.body;
+        await syncServices.updateRemoteWithLocalQueries(localQueries);
+        res.status(200).json({
+            message: 'Remote database updated successfully'
+        });
+    } catch (error) {
+        console.error(error)
+        res.status(500).json('Error in getQuerysToSyncRemote function');
+    }
+}
+
+
+export default { getQuerysToSyncLocal, getQuerysToSyncRemote };
