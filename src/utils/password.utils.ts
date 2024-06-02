cbbd4282888ef7e13d7b483aff864cc811b9ab14
Steven <maxpowersga@gmail.com>
2024-06-02 17:27:18 -0500

    actualizacion de encriptado

--- src/utils/password.utils.ts
+++ src/utils/password.utils.ts
@@ -1,12 +1,11 @@
-const bcrypt = require('bcryptjs');
-
-const SALT_ROUNDS = 10;
+const crypto = require('crypto');
 
 export const encryptPassword = async (password: string): Promise<string> => {
-   const salt = await bcrypt.genSalt(SALT_ROUNDS);
-   return await bcrypt.hash(password, salt);
+   const hash = crypto.createHash('sha256').update(password).digest('hex');
+   return hash;
 }
 
 export const comparePassword = async (password: string, hash: string): Promise<boolean> => {
-   return await bcrypt.compare(password, hash);
+   const hashVerify = crypto.createHash('sha256').update(password).digest('hex');
+  return hash === hashVerify;
 }
